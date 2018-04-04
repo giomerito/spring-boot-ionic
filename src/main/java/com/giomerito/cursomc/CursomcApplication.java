ffb2a92446fd59c33611bed004202b3cd0a23908
Giomerito Alves de Souza <giomerito.souza@gmail.com>
2018-04-03 22:14:49 -0300

    Projeto criado

---
+++ src/main/java/com/giomerito/cursomc/CursomcApplication.java
@@ -0,0 +1,14 @@
+package com.giomerito.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcApplication.class, args);
+	}
+}
+
+//Iniciar um repositorio de versionamento
