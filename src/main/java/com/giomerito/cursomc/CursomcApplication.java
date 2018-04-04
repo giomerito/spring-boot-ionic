bea79f257baf4f541c308054f771d78d56f864f8
Giomerito Alves de Souza <giomerito.souza@gmail.com>
2018-04-04 08:04:45 -0300

    Criando operacao de instanciacao

--- src/main/java/com/giomerito/cursomc/CursomcApplication.java
+++ src/main/java/com/giomerito/cursomc/CursomcApplication.java
@@ -1,14 +1,35 @@
 package com.giomerito.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.giomerito.cursomc.domain.Categoria;
+import com.giomerito.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.save(Arrays.asList(cat1, cat2));
+	}
+	
+	
 }
 
 //Iniciar um repositorio de versionamento
