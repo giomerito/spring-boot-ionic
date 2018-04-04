d66b87e294f8ed07eaedb737c0de451c560a483f
Giomerito Alves de Souza <giomerito.souza@gmail.com>
2018-04-04 10:36:51 -0300

    Estado e Cidade

---
+++ src/main/java/com/giomerito/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.giomerito.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.giomerito.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+	//Esta interface fará todas as operações referente a classe passada e o Id
+}
