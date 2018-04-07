a410aa8e44088f63c613c77d8d98bb4b83cfb758
Giomerito Alves de Souza <giomerito.souza@gmail.com>
2018-04-07 19:32:56 -0300

    Validacao customiza email nao repetido na insercao do cliente

--- src/main/java/com/giomerito/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/giomerito/cursomc/repositories/ClienteRepository.java
@@ -2,10 +2,14 @@ package com.giomerito.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.giomerito.cursomc.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
 	//Esta interface fará todas as operações referente a classe passada e o Id
+	
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
 }
--- src/main/java/com/giomerito/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/giomerito/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.giomerito.cursomc.domain.Cliente;
 import com.giomerito.cursomc.domain.enums.TipoCliente;
 import com.giomerito.cursomc.dto.ClienteNewDTO;
+import com.giomerito.cursomc.repositories.ClienteRepository;
 import com.giomerito.cursomc.resources.exceptions.FieldMessage;
 import com.giomerito.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
 
 	@Override
 	public void initialize(ClienteInsert ann) {
@@ -31,8 +38,12 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
 		}
-
-		// inclua os testes aqui, inserindo erros na lista
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail()); 
+		if(aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}
+		
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
 			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
