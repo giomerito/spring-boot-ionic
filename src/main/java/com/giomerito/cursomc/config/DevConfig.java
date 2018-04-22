7a7997fae270355c021f5dd45bf1e1ef200d8730
Giomerito Alves de Souza <giomerito.souza@gmail.com>
2018-04-21 22:42:19 -0300

    Implementando SmtpService com servidor do Google

--- src/main/java/com/giomerito/cursomc/config/DevConfig.java
+++ src/main/java/com/giomerito/cursomc/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.giomerito.cursomc.services.DBService;
+import com.giomerito.cursomc.services.EmailService;
+import com.giomerito.cursomc.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -31,5 +33,10 @@ public class DevConfig {
 		}
 		dbService.instantitateDatabase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 }
