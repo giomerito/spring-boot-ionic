cf1017bbd90c9e70716dad8e526e07bc8f87d409
Giomerito Alves de Souza <giomerito.dev@gmail.com>
2018-05-19 18:08:59 -0300

    Restricao de conteudo: cliente recupera apenas seus pedidos

--- src/main/java/com/giomerito/cursomc/repositories/PedidoRepository.java
+++ src/main/java/com/giomerito/cursomc/repositories/PedidoRepository.java
@@ -1,11 +1,18 @@
 package com.giomerito.cursomc.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.giomerito.cursomc.domain.Cliente;
 import com.giomerito.cursomc.domain.Pedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Integer>{
 	//Esta interface fará todas as operações referente a classe passada e o Id
+	
+	@Transactional(readOnly=true)
+	Page<Pedido> findByCliente(Cliente cliente, Pageable pageRequest);
 }
--- src/main/java/com/giomerito/cursomc/services/PedidoService.java
+++ src/main/java/com/giomerito/cursomc/services/PedidoService.java
@@ -3,16 +3,23 @@ package com.giomerito.cursomc.services;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.giomerito.cursomc.domain.Cliente;
 import com.giomerito.cursomc.domain.ItemPedido;
 import com.giomerito.cursomc.domain.PagamentoComBoleto;
 import com.giomerito.cursomc.domain.Pedido;
 import com.giomerito.cursomc.domain.enums.EstadoPagamento;
+import com.giomerito.cursomc.repositories.ClienteRepository;
 import com.giomerito.cursomc.repositories.ItemPedidoRepository;
 import com.giomerito.cursomc.repositories.PagamentoRepository;
 import com.giomerito.cursomc.repositories.PedidoRepository;
+import com.giomerito.cursomc.security.UserSS;
+import com.giomerito.cursomc.services.exceptions.AuthorizationException;
 import com.giomerito.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -32,6 +39,8 @@ public class PedidoService {
 	private ClienteService clienteService;
 	@Autowired
 	private EmailService emailService;
+	@Autowired
+	private ClienteRepository clienteRepository;
 	
 	public Pedido find(Integer id) {
 		Pedido obj = repo.findOne(id);
@@ -63,6 +72,16 @@ public class PedidoService {
 		itemPedidoRepository.save(obj.getItens());
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
+	} 
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		UserSS user = UserService.authenticated();
+		if(user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteRepository.findOne(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
 	}
 
 }
