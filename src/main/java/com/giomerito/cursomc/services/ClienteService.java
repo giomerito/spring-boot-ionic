17cbadd80749f9785435542d90257352ce2c6362
Giomerito Alves de Souza <giomerito.dev@gmail.com>
2018-05-20 22:04:43 -0300

    Endpoint para buscar cliente por email

--- src/main/java/com/giomerito/cursomc/services/ClienteService.java
+++ src/main/java/com/giomerito/cursomc/services/ClienteService.java
@@ -92,6 +92,20 @@ public class ClienteService {
 	public List<Cliente> findAll() {
 		return repo.findAll();
 	}
+	
+	public Cliente findByEmail(String email) {
+		UserSS user = UserService.authenticated();
+		if(user == null || !user.hasRole(Perfil.ADMIN) && !email.equals(user.getUsername())) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		
+		Cliente obj = repo.findByEmail(email);
+		if(obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! id: " + user.getId()
+			+ ", Tipo: " + Cliente.class.getName());
+		}
+		return obj;
+	}
 
 	// Metodo de paginação
 	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
